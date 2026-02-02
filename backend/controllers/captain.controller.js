d220ee052d4e32b19914a0fd2a96a565f3098521
ajeetyadav7464 <ajeetyadav7464@gmail.com>
2026-02-02 22:17:31 +0530

    Update captain.controller.js

--- backend/controllers/captain.controller.js
+++ backend/controllers/captain.controller.js
@@ -1,3 +1,4 @@
+dkjklrtyuioprtyui
 const captainModel = require('../models/captain.model');
 const captainService = require('../services/captain.service');
 const blackListTokenModel = require('../models/blackListToken.model');
